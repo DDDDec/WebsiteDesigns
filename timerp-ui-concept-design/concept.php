90ee943a57bb8d96782f0bebc809bbf90f81ac7c
Dec <declandavidwww@gmail.com>
2025-05-28 21:09:05 +0100

    middle menu dropper
    middle menu dropper

--- timerp-ui-concept-design/concept.php
+++ timerp-ui-concept-design/concept.php
@@ -7,6 +7,29 @@
         <img src="https://sandbox.habbo.com/habbo-imaging/avatarimage?size=l&figure=hd-3813-0-0.ha-5218-0-0.he-4288-0-0.ea-3822-0-0.fa-5211-0-0.ch-5904-0-0.cp-5979-0-0.cc-3066-0-0.ca-5414-0-0.lg-4083-0-0.sh-3206-0-0.wa-5782-0-0.hr-5841-0-0.&gesture=std&action=std&direction=2&head_direction=2&headonly=0&gender=M"/>
     </div>
     <img class="flip" src="bg_bottomright.0bb434c7.png"/>
+    <div style="right: 10px;" class="avatar-grid">
+        <div class="avatar-grid-a-1">
+            <div class="circle-menu">
+                <div style="float: right;" class="bar">
+                    Declan <div class="rank">Police</div>
+                </div>
+            </div>
+        </div>
+        <div class="avatar-grid-a-1">
+            <div class="circle-menu">
+                <div class="description">
+                    Welcome to TimeRP
+                </div>
+            </div>
+        </div>
+        <div class="avatar-grid-a-1">
+            <div style="text-align: right;" class="circle-menu">
+                <a><i class="fa-solid fa-user"></i></a>
+                <a><i class="fa-solid fa-gun"></i></a>
+                <a><i class="fa-solid fa-heart"></i></a>
+            </div>
+        </div>
+    </div>
 </div>
 
 <div class="avatar-placeholder right">
@@ -17,24 +40,36 @@
     <div class="avatar-grid">
         <div class="avatar-grid-a-1">
             <div class="circle-menu">
-                <a></a>
-                <a></a>
-                <a></a>
+                <div class="bar">
+                    Declan <div class="rank">Gangster</div>
+                </div>
             </div>
         </div>
         <div class="avatar-grid-a-1">
             <div class="circle-menu">
-                <a></a>
-                <a></a>
-                <a></a>
+                <div class="description">
+                    Welcome to TimeRP
+                </div>
             </div>
         </div>
         <div class="avatar-grid-a-1">
             <div class="circle-menu">
                 <a><i class="fa-solid fa-user"></i></a>
-                <a></a>
-                <a></a>
+                <a><i class="fa-solid fa-gun"></i></a>
+                <a><i class="fa-solid fa-heart"></i></a>
             </div>
+        </div>
+    </div>
+</div>
+
+<div class="quick-menu">
+    <div class="button">
+        <div class="box">
+            <a class="button-2"></a>
+            <a class="button-3"></a>
+            <a class="button-4"></a>
+            <a class="button-5"></a>
+            <a class="button-6"></a>
         </div>
     </div>
 </div>
